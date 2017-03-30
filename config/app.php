b39a05ffedb09b24d0a439f8de3d4ed9a33fa61c
hotsaucejake <get@hotsaucejake.com>
2017-03-30 00:15:42 +0000

    install socialite
    - Reason(s):
    
    - Change(s):
    
    - Reference(s):

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'Laravel',
+    'name' => 'Larazure',
 
     /*
     |--------------------------------------------------------------------------
@@ -176,6 +176,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        
+        \SocialiteProviders\Manager\ServiceProvider::class,
 
     ],
 
@@ -225,6 +227,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        
+        'Socialite' => Laravel\Socialite\Facades\Socialite::class,
 
     ],
 
