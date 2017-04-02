c28ef8c9d56af4203622542047e8ef92e15bfbdf
hotsaucejake <get@hotsaucejake.com>
2017-04-02 21:47:58 +0000

    add deactivated role to seeder
    - Reason(s):
    
    - Change(s):
    
    - Reference(s):

--- database/seeds/UserRolesSeeder.php
+++ database/seeds/UserRolesSeeder.php
@@ -25,5 +25,10 @@ class UserRolesSeeder extends Seeder
             'name' => 'Member',
             'slug' => 'member',
          ]);
+         
+         \HttpOz\Roles\Models\Role::create([
+            'name' => 'Deactivated',
+            'slug' => 'deactivated',
+         ]);
     }
 }
