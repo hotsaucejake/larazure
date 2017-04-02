1873bb937da5ce3de9463bfa9bc6a95e129c8807
hotsaucejake <get@hotsaucejake.com>
2017-04-02 19:31:04 +0000

    register the roles service provider
    - Reason(s):
    
    - Change(s):
    
    - Reference(s):

--- config/app.php
+++ config/app.php
@@ -178,6 +178,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         
         \SocialiteProviders\Manager\ServiceProvider::class,
+        HttpOz\Roles\RolesServiceProvider::class,
 
     ],
 
